7764742a2501e6ca50e4ab0c60b529c0f017a207
sb1020 <hihihiha101011@gmail.com>
2024-05-17 09:26:03 +0530

    Update bytecount.c

--- bytecount.c
+++ bytecount.c
@@ -1,36 +1,41 @@
-#include<stdio.h>
-#include<string.h>
-char data [8];
+#include <stdio.h>
+#include <string.h>
+
+char data[8];
 char datawithLen[9];
-void sender(){
+
+void sender() {
     printf("\nSender\n-------------------------------------");
 
-    printf("\nEnter data :");
-    scanf("%s",data);
+    printf("\nEnter data: ");
+    scanf("%s", data);
 
     int len = strlen(data);
-    datawithLen[0] = (len+1) + '0'; // char to int => add '0' to char
- 
-    for(int i = 0 ; i < len ; i++){
-        datawithLen[i+1] = data[i];
+    datawithLen[0] = len + '0'; // Store the length of data as a character
+
+    for(int i = 0; i < len; i++) {
+        datawithLen[i + 1] = data[i];
     }
 
-    datawithLen[len+1] ='\0';
-    printf("\nData to send : ");
-    for(int i = 0 ; i <= len ; i++){
-        printf("%c ",datawithLen[i]);
+    datawithLen[len + 1] = '\0';
+    printf("\nData to send: ");
+    for(int i = 0; i <= len; i++) {
+        printf("%c ", datawithLen[i]);
     }
 }
 
-void receiver(char datawithLen[]){
+void receiver(char datawithLen[]) {
     printf("\n\nReceiver\n-------------------------------------");
-    int lenofDataReceived = datawithLen[0] - '0'; // char to integer => subtract '0' from char
-    printf("\nOriginal Data : ");
-    for(int i = 1 ; i <= lenofDataReceived ; i++){
-        printf("%c ",datawithLen[i]);
+    int lenofDataReceived = datawithLen[0] - '0'; // Convert char back to integer
+    printf("\nOriginal Data: ");
+    for(int i = 1; i <= lenofDataReceived; i++) {
+        printf("%c ", datawithLen[i]);
     }
+    printf("\n");
 }
-int main(){
+
+int main() {
     sender();
     receiver(datawithLen);
+    return 0;
 }
