7bef92740725415516cc7a04f69bf5c942616fb2
1020 <hihihiha101011@gmail.com>
2024-05-17 12:51:54 +0530

    Update goback.c

--- goback.c
+++ goback.c
@@ -1,36 +1,54 @@
+
+#include <math.h>
 #include <stdio.h>
 
-int main() {
-    int windowsize, totalPackets, sent = 0, ack, i;
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
-    // Input total number of packets
-    printf("Enter total number of packets to be sent: ");
-    scanf("%d", &totalPackets);
+int tt, tp, size;
+int windowIdx = 0, time = 0;
 
-    // Input window size
-    printf("Enter window size: ");
-    scanf("%d", &windowsize);
-
-    while (sent < totalPackets) {
-        // Transmit frames within the window
-        for (i = 0; i < windowsize && sent < totalPackets; i++) {
-            printf("Frame %d has been transmitted.\n", sent);
-            sent++;
-        }
-
-        // Acknowledgement input
-        printf("\nPlease enter the last Acknowledgement received.\n");
-        scanf("%d", &ack);
-
-        // If the last acknowledged packet equals the total packets, break
-        if (ack >= totalPackets) {
-            printf("All packets have been acknowledged.\n");
-            break;
-        } else {
-            // Move the sent pointer to the last acknowledged packet
-            sent = ack;
-        }
+void sendWindow() {
+    int packets = windowIdx + size;
+    while (windowIdx < packets) {
+        time += tt;
+        printf("Sending packet %d at time %d ms.\n", windowIdx++ + 1, time);
     }
+}
 
-    return 0;
+int main(void) {
+    printf("Enter the transmission time: ");
+    scanf("%d", &tt);
+
+    printf("Enter the propogation time: ");
+    scanf("%d", &tp);
+
+    printf("Enter the window size: ");
+    scanf("%d", &size);
+
+    // Ask user for total packets to send.
+    int totalPackets = 10;
+
+    sendWindow();
+    time += tp - (size - 1) * tt;
+
+    fflush(stdin);
+    do {
+        char ack;
+        printf("Do you want to acknowledge packet %d (y/n): ", windowIdx - size + 1);
+        scanf(" %c", &ack);
+
+        if (ack == 'y') {
+            printf("Acknowledgement for packet %d receieved at %d ms.\n", windowIdx - size + 1,
+                   time);
+
+            time += tt;
+            printf("Sending packet %d at %d ms.\n", windowIdx++ + 1, time);
+        } else {
+            printf("Not received acknowledgement for packet %d, waited till %d ms.\n",
+                   windowIdx - size + 1, time);
+
+            windowIdx -= size;
+            sendWindow();
+        }
+    } while (windowIdx < totalPackets);
 }
